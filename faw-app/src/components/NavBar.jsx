bcbb8defaf9cfddd1e25a15ee79447b864f7cb83
annika truong <annikatruongt@gmail.com>
2021-12-21 16:58:07 -0800

    styled nav

--- faw-app/src/components/NavBar.jsx
+++ faw-app/src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import '../App.css'
 export default function NavBar(){
     return (
         <div className='logo'>
-                <img src={'./fawLogo.jpg'} alt='logo' className = 'logo-img' />
+                <img src={'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa9%2F40%2F45%2Fa94045632d0ec2867d0a4d5a31013407.png&f=1&nofb=1'} alt='logo' className = 'logo-img' />
         <div className='navbar'>
             <Link to ='/' className='link'> Home </Link>
             <Link to ='/waifu' className= 'link'> Waifu </Link>
