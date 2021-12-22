95e317174534830b2d8e817485a749908d962287
annika truong <annikatruongt@gmail.com>
2021-12-22 13:26:31 -0800

    styled a section on the home

--- faw-app/src/routes/Home.js
+++ faw-app/src/routes/Home.js
@@ -4,18 +4,21 @@ import Gallery from '../components/Slider'
 
 export default function Home(props) {
     return (
+        <div className='layout'>
         <div className='home'>
             <div className='intro'>
-                <h1>welcome ʕʘ̅͜ʘ̅ʔ</h1>
-                <p>find your waifu and choose a gift for her. don't like one? modify. really don't like one? DELETE.</p>
-                <p>below are some of the greatest waifu's</p>
+                <div className='home-intro'>
+                    <h1 className='welcome'>welcome ʕʘ̅͜ʘ̅ʔ</h1>
+                    <p>find your waifu and choose a gift for her. don't like one? modify. really don't like one? DELETE.</p>
+                    <p>below are some of the greatest waifu's</p>
+                </div>
                 <div className='btns-wg'>
-                    <button className='btn-waifu' onClick={event => window.location.href='/waifu'}>( ˘ ³˘) find my waifu</button>
-                    <span className='space'></span>
-                    <button className='btn-gift' onClick={event => window.location.href='/gift'}>( ﾟｰﾟ) find my gift</button>
+                    <button className='btn btn-waifu' onClick={event => window.location.href='/waifu'}>( ˘ ³˘) find my waifu</button>
+                    <button className='btn btn-gift' onClick={event => window.location.href='/gift'}>( ﾟｰﾟ) find my gift</button>
                 </div>
             </div>
             <Gallery />
+        </div>
         </div>
     )
 }
