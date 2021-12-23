e427efd0bd0d051e5e9887989dc083ce0ff39ff1
annika truong <annikatruongt@gmail.com>
2021-12-23 08:42:28 -0800

    styled about page. project finished

--- faw-app/src/routes/About.js
+++ faw-app/src/routes/About.js
@@ -3,16 +3,21 @@ import { Link } from 'react-router-dom'
 
 export default function About(props) {
     return (
-        <div>
+        <div className='about-page'>
+            <div className='about-intro'>
+            <img src='uwu.png' className='uwu' />
+            <div className='about-words'>
             <h1>about (´◡`)</h1>
             <h2>how to use ٩(｡•́‿•̀｡)۶</h2>
-            <h3>FAW is the best app to find the waifu of your preferred taste >:P
-                Create, update, and delete waifu's you hate, love, or don't see. yeah, do the same for the gifts too.
+            <h3 className='about'>FAW is the best app to find the waifu of your preferred taste >:P<br />
+                Create, update, and delete waifu's you hate, love, or don't see. <br /> yeah, do the same for the gifts too.
             </h3>
-            <h2>who made this ¯\_(ツ)_/¯</h2>
-            <h3>well since you're reading this far I guess I'll tell you about myself. my name is annika. 
-                actually, here are just some links to my socials.
+            <h2 className='who'>who made this ¯\_(ツ)_/¯</h2>
+            <h3 className='about'>well since you're reading this far I guess I'll tell you about myself. my name is annika. 
+            <br /> actually, here are just some links to my socials.
             </h3>
+            </div>
+            </div>
         <div className='socials'>
             <a href='https://github.com/atruong0914'>
                 <img src='./github.png' className='icon' />
